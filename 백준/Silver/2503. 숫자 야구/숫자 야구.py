f3fa7b6caa3901e71aa0b910cb59474454696d8e
Lee Kyungmin <74596625+kyungmin1212@users.noreply.github.com>
2023-10-10 06:28:46 +0900

    [Silver III] Title: 숫자 야구, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.py
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.py
@@ -4,6 +4,18 @@ arr = [list(map(int,input().split())) for _ in range(n)]
 
 answer = 0
 
+def check_strike_ball(candidate,check_num):
+    strike, ball = 0,0
+    for i in range(3):
+        for j in range(3):
+            if candidate[i] == check_num[j]:
+                if i==j:
+                    strike+=1
+                else:
+                    ball+=1
+    return strike, ball
+
+
 for num in range(100,1000):
     str_num = str(num).zfill(3)
 
@@ -13,41 +25,9 @@ for num in range(100,1000):
     flag = True
     for check_num, s, b in arr:
         check_num = str(check_num)
-        if str_num[0]==check_num[0]:
-            s-=1
-            if s<0:
-                flag = False
-                break
-        elif str_num[0]==check_num[1] or str_num[0]==check_num[2]:
-            b-=1
-            if b<0:
-                flag = False
-                break
-
-        if str_num[1]==check_num[1]:
-            s-=1
-            if s<0:
-                flag = False
-                break
-        elif str_num[1]==check_num[0] or str_num[1]==check_num[2]:
-            b-=1
-            if b<0:
-                flag = False
-                break
-
-        if str_num[2]==check_num[2]:
-            s-=1
-            if s<0:
-                flag = False
-                break
-        elif str_num[2]==check_num[0] or str_num[2]==check_num[1]:
-            b-=1
-            if b<0:
-                flag = False
-                break
-
-        if s!=0 or b!=0:
-            flag = False
+        strike,ball = check_strike_ball(str_num,check_num)
+        if s!=strike or b!=ball:
+            flag=False
             break
     if flag:
         answer+=1
