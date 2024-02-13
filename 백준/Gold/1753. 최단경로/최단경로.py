4ecc2921477806f7a3c0ee85af18f1f56d494262
Lee Kyungmin <74596625+kyungmin1212@users.noreply.github.com>
2024-02-13 23:36:06 +0900

    [Gold IV] Title: 최단경로, Time: 604 ms, Memory: 68552 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,37 +1,36 @@
 import sys
-from heapq import heappush,heappop
+from heapq import heappush, heappop
 
-input  = sys.stdin.readline
+input = sys.stdin.readline
 
-v,e = map(int,input().split())
-
+v, e = map(int, input().split())
 start_node = int(input())
-
-graph = [[] for _ in range(v+1)]
+graph = [[] for _ in range(v + 1)]
 
 for _ in range(e):
-    a,b,w = map(int,input().split())
-    graph[a].append((b,w))
-    
+    a, b, w = map(int, input().split())
+    graph[a].append((b, w))
 
-def dijstra(start):
+def dijkstra(start):
     q = []
-    answer = [float('inf') for _ in range(v+1)]
-    heappush(q,(0,start))
+    distance = [float('inf')] * (v + 1)
+    distance[start] = 0
+    heappush(q, (0, start))
+    
     while q:
+        curr_dist, curr_node = heappop(q)
 
-        now_weight,now_node = heappop(q)
-        if answer[now_node]==float('inf'):
-            answer[now_node]=now_weight
-        else:
+        if curr_dist > distance[curr_node]:
             continue
-        
-        for next_node,weight in graph[now_node]:
-            if answer[next_node]==float('inf'):
-                heappush(q,(now_weight+weight,next_node))
-                
-    return answer
 
-answer = [item if item!=float('inf') else 'INF' for item in dijstra(start_node)]
-for item in answer[1:]:
-    print(item)
+        for next_node, weight in graph[curr_node]:
+            next_dist = curr_dist + weight
+            if next_dist < distance[next_node]:
+                distance[next_node] = next_dist
+                heappush(q, (next_dist, next_node))
+                
+    return distance
+
+distances = dijkstra(start_node)
+for dist in distances[1:]:
+    print(dist if dist != float('inf') else 'INF')
