4fd125b2cac750f92f0f758a0260de60319afd57
Lee Kyungmin <74596625+kyungmin1212@users.noreply.github.com>
2024-03-16 18:19:40 +0900

    [Silver II] Title: 랜선 자르기, Time: 60 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,17 +1,25 @@
 import sys
-k,n=map(int,sys.stdin.readline().split())
-arr=[]
-for i in range(k):
-    arr.append(int(sys.stdin.readline()))
-lower_bound,upper_bound=1,max(arr)
-result = 0
 
-while lower_bound<=upper_bound:
-    mid=(lower_bound+upper_bound)//2
-    cut_sum=sum([(i//mid) for i in arr])
-    if cut_sum>=n:
-        result=mid
-        lower_bound=mid+1
-    elif cut_sum<n:
-        upper_bound=mid-1
-print(result)
+input = sys.stdin.readline
+
+k, n = map(int, input().split())
+
+arr = [int(input()) for _ in range(k)]
+
+
+def find(arr, n):
+    start, end = 1, max(arr) + 1
+
+    while start < end:
+        mid = (start + end) // 2
+
+        output = sum([item // mid for item in arr])
+        if output >= n:
+            start = mid + 1
+        else:
+            end = mid
+
+    return start - 1
+
+
+print(find(arr, n))
