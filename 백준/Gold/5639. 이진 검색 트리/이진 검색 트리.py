03996ce64404f639a25abeb11da92c4ca09ed759
Lee Kyungmin <74596625+kyungmin1212@users.noreply.github.com>
2024-03-19 03:12:10 +0900

    [Gold V] Title: 이진 검색 트리, Time: 1744 ms, Memory: 33628 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -11,37 +11,22 @@ while True:
         break
 
 
-class Node:
-    def __init__(self, value, left=None, right=None):
-        self.value = value
-        self.left = left
-        self.right = right
+def postorder(start, end):  # start~ end까지 탐색
+    if start > end:
+        return
+
+    # start가 가장 클 경우,
+    # 오른쪽 서브 트리는 없기 때문에 없는 값인 end+1로 지정해주기
+    div = end + 1
+    root = arr[start]  # start 는 root 노드
+    for i in range(start + 1, end + 1):
+        if root < arr[i]:
+            div = i  # i 부터 오른쪽 서브트리입니다.
+            break
+
+    postorder(start + 1, div - 1)  # 왼쪽 서브트리
+    postorder(div, end)  # 오른쪽 서브트리
+    print(arr[start])
 
 
-def add_node(root, x):
-    if root.value < x:
-        if root.right:
-            add_node(root.right, x)
-        else:
-            root.right = Node(value=x)
-    else:
-        if root.left:
-            add_node(root.left, x)
-        else:
-            root.left = Node(value=x)
-
-
-root = Node(value=arr[0])
-for item in arr[1:]:
-    add_node(root, item)
-
-
-def postorder(root):
-    if root:
-        postorder(root.left)
-        postorder(root.right)
-        print(root.value)
-    return
-
-
-postorder(root)
+postorder(0, len(arr) - 1)
