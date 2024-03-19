c80f2d42463dba706f95f5653fda2c1a21934420
Lee Kyungmin <74596625+kyungmin1212@users.noreply.github.com>
2024-03-20 02:11:25 +0900

    [level 5] Title: 재밌는 레이싱 경기장 설계하기, Time: 69.92 ms, Memory: 24.7 MB -BaekjoonHub

--- 프로그래머스/5/214292. 재밌는 레이싱 경기장 설계하기/재밌는 레이싱 경기장 설계하기.py
+++ 프로그래머스/5/214292. 재밌는 레이싱 경기장 설계하기/재밌는 레이싱 경기장 설계하기.py
@@ -14,18 +14,22 @@ def solution(heights):
             arr.append(heights[i])
             arr.append(heights[n + is_odd + i])
         arr.append(heights[n])
+        # 인접한 높이 차이를 계산합니다.
+        arr.append(arr[0])  # 배열의 처음과 끝을 연결하기 위해
+        diff = [abs(arr[j] - arr[j + 1]) for j in range(len(arr) - 1)]
+        diff.sort()
+        answer = diff[0] if len(diff) == 1 else diff[1]
     else:
         for i in range(n):
             arr.append(heights[n + is_odd + i])
             arr.append(heights[i])
+        diff = [abs(arr[j] - arr[j + 1]) for j in range(len(arr) - 1)]
+        diff.sort()
+        answer = diff[0]
 
-    # 인접한 높이 차이를 계산합니다.
-    arr.append(arr[0])  # 배열의 처음과 끝을 연결하기 위해
-    diff = [abs(arr[j] - arr[j + 1]) for j in range(len(arr) - 1)]
 
     # 높이 차이 배열을 정렬합니다.
-    diff.sort()
-    answer = diff[0] if len(diff) == 1 else diff[1]
+    
 
     return answer
 
