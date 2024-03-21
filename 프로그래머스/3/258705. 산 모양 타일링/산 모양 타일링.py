302de54d241a7ce5656820627d6872dd1d789c3a
Lee Kyungmin <74596625+kyungmin1212@users.noreply.github.com>
2024-03-22 05:15:14 +0900

    [level 3] Title: 산 모양 타일링, Time: 38.53 ms, Memory: 14.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.py
@@ -0,0 +1,20 @@
+import math
+
+def solution(n, tops):
+    
+    dp = [0 for _ in range(n*2+1)]
+    if tops[0]==1:
+        dp[1]=2
+    else:
+        dp[1]=1
+    for i in range(2,n*2+1):
+        if i%2==1:
+            if tops[i//2]==1:
+                dp[i]=dp[i-1]+dp[i-2]+1+dp[i-1]+1
+            else:
+                dp[i]=dp[i-1]+dp[i-2]+1
+        else:
+            dp[i]=dp[i-1]+dp[i-2]+1
+        dp[i]=dp[i]%10007
+                
+    return (dp[-1]+1)%10007
