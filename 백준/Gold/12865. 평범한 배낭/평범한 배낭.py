226861be5386d3cc0ae8b8947f6989070ccc6382
Lee Kyungmin <74596625+kyungmin1212@users.noreply.github.com>
2024-03-21 21:03:09 +0900

    [Gold V] Title: 평범한 배낭, Time: 3404 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -2,15 +2,14 @@ import sys
 
 input = sys.stdin.readline
 
-n,k = map(int,input().split())
+n, k = map(int, input().split())
 
-arr = [list(map(int,input().split())) for _ in range(n)]
+arr = [list(map(int, input().split())) for _ in range(n)]
 
-dp = [0 for _ in range(k+1)]
+dp = [0 for _ in range(k + 1)]
 
-for w,v in arr:
-    for i in reversed(range(1,k+1)):
-        if i>=w:
-            dp[i]=max(dp[i],dp[i-w]+v)
+for w, v in arr:
+    for i in reversed(range(w, k + 1)):
+        dp[i] = max(dp[i], dp[i - w] + v)
 
-print(dp[-1])
+print(max(dp))
