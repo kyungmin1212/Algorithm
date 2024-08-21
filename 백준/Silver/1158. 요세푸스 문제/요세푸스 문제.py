284873f39b350947b2222d7c907fe628a2f42613
Lee Kyungmin <74596625+kyungmin1212@users.noreply.github.com>
2024-08-21 13:45:37 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 4216 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -5,16 +5,14 @@ n, k = map(int, input().split())
 q = deque([i for i in range(1, n + 1)])
 
 count = 1
-answer = "<"
+answer = []
 while q:
     if count == k:
         count = 1
-        if len(q) > 1:
-            answer += f"{str(q.popleft())}, "
-        else:
-            answer += f"{str(q.popleft())}>"
+        answer.append(str(q.popleft()))
+
     else:
         count += 1
         q.append(q.popleft())
 
-print(answer)
+print("<" + ", ".join(answer) + ">")
