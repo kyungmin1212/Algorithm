635d57e4f5a18383724847e57d14fd91b8fa1535
Lee Kyungmin <74596625+kyungmin1212@users.noreply.github.com>
2024-08-21 13:46:53 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 56 ms, Memory: 34008 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -4,15 +4,10 @@ n, k = map(int, input().split())
 
 q = deque([i for i in range(1, n + 1)])
 
-count = 1
 answer = []
-while q:
-    if count == k:
-        count = 1
-        answer.append(str(q.popleft()))
 
-    else:
-        count += 1
-        q.append(q.popleft())
+while q:
+    q.rotate(-(k - 1))
+    answer.append(str(q.popleft()))
 
 print("<" + ", ".join(answer) + ">")
